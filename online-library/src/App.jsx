5c24d9b192ee474b8c3b202a463aa2a769ba400e
sufalthakre <sufal@tellistechnologies.com>
2026-05-29 15:33:14 +0530

    implement routing and add initial page components for the online library

--- online-library/src/App.jsx
+++ online-library/src/App.jsx
@@ -1,11 +1,41 @@
+import { BrowserRouter, Routes, Route, Outlet } from 'react-router-dom';
+
+import Navbar from './components/Navbar';
+
+import Home from './pages/Home';
+import BrowseBooks from './pages/BrowseBooks';
+import BookDetails from './pages/BookDetails';
+import AddBook from './pages/AddBook';
+import NotFound from './pages/NotFound';
 
 function App() {
-  
   return (
-    <>
-    <h1 className="bg-amber-500 text-3xl w-full p-3">hello world</h1>
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+
+        {/* Pages with Navbar */}
+        <Route
+          element={<>
+          <Navbar />
+          <Outlet />
+          <footer className="bg-forest-800 text-white/50 text-sm text-center py-5">
+                © 2025 Bibliotheca
+          </footer>
+          </>}
+        >
+          <Route path="/" element={<Home />} />
+          <Route path="/books" element={<BrowseBooks />} />
+          <Route path="/books/:category" element={<BrowseBooks />} />
+          <Route path="/books/details/:id" element={<BookDetails />} />
+          <Route path="/add-book" element={<AddBook />} />
+        </Route>
+
+        {/* 404 Page */}
+        <Route path="*" element={<NotFound />} />
+
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
